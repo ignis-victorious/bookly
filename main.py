d0ed7a8ba1eebb64d1cfc9205b6babb3cb10f513
Emagnu <ignisvictorious@gmail.com>
2025-04-07 03:00:51 +0100

    add get endpoints

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #  ___________________
 #  Import LIBRARIES
+from typing import Any
 from fastapi import FastAPI
 #  Import FILES
 #  ___________________
@@ -21,6 +22,17 @@ async def greet_name(name: str) -> dict[str, str]:
 @app.get("/greetings")
 async def greetings_w_name(name: str) -> dict[str, str]:
     return {"message": f"Hello {name}"}
+
+
+@app.get("/mix-greet/{name}")
+# async def greetings_mix(name: str, age: int) -> dict[str, int]:
+async def greetings_mix(name: str, age: int = None) -> dict[str, Any]:
+    return {"message": f"Hello {name}", "age: ": age}
+
+
+@app.get("/optional-greet")
+async def greetings_opt(name: str = "User", age: int = None) -> dict[str, Any]:
+    return {"message": f"Hello {name}", "age: ": age}
 
 
 # def main():
